c3592eb6229be5a3c3b6782a72aba58a01681769
Gerald-Bartlett <96056847+Gerald-Bartlett@users.noreply.github.com>
2022-05-18 09:09:23 -0230

    Initial commit

---
+++ src/test/java/com/keyin/demo/DemoTest.java
@@ -0,0 +1,13 @@
+package com.keyin.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+
+    @Test
+    public void testReturnsTrue(){
+        Assertions.assertTrue(new Demo().returnTrue());
+    }
+}
